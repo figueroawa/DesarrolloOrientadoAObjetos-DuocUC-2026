26648f684cbea5efed406b7f34b074a11a8fac09
Selena Figueroa <selena.ale.figueroa@gmail.com>
2026-08-11 17:05:38 -0400

    Clase 11/08 - Clases, comportamientos e instancias de clase

---
+++ src/Perro.java
@@ -0,0 +1,11 @@
+public class Perro {
+    String nombre;
+    String raza;
+    int edad;
+    float peso;
+    boolean vacuna;
+
+    public String ladrar() {
+        return "Woof woof!";
+    }
+}
---
+++ src/PerroSinPoo.java
@@ -0,0 +1,21 @@
+public class PerroSinPoo {
+    public static void main(String[] args) {
+        String nombre = "Uika";
+        String raza = "Golden Retriever";
+        int edad = 15;
+        float peso = 22.3f;
+        boolean vacuna = true;
+
+        System.out.println("Woof!");
+        System.out.println("Nombre del perro:");
+        System.out.println(nombre);
+        System.out.println("Raza del perro:");
+        System.out.println(raza);
+        System.out.println("Edad del perro:");
+        System.out.println(edad);
+        System.out.println("Peso del perro:");
+        System.out.println(peso);
+        System.out.println("¿Está vacunado?");
+        System.out.println(vacuna);
+    }
+}
